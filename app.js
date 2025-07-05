57de5126121366ed0ac37a18536cd957ccde477a
vrahkonenn <vrahkonen02@gmail.com>
2025-07-05 23:07:21 +0300

    Update app.js

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ mongoose
   })
 
 app.use(express.json())
+app.use(express.static('dist'))
 app.use(middleware.requestLogger)
 app.use('/api/blogs', blogsRouter)
 app.use(middleware.unknownEndpoint)
